23518999fdf56f138921c4de2c29df38be669c82
Monika Georgieva <3731686+spiralnegative@users.noreply.github.com>
2026-08-20 22:32:01 +0300

    Replace jquery with vanilla js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,98 +1,119 @@
 /**
- * Main JS file for Casper behaviours
+ * Main JS file for theme behaviours (dependency-free)
  */
-
-/* globals jQuery, document */
-(function ($, sr, undefined) {
+(function () {
     "use strict";
 
-    var $document = $(document),
-
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        debounce = function (func, threshold, execAsap) {
-            var timeout;
-
-            return function debounced () {
-                var obj = this, args = arguments;
-                function delayed () {
-                    if (!execAsap) {
-                        func.apply(obj, args);
-                    }
-                    timeout = null;
-                }
-
-                if (timeout) {
-                    clearTimeout(timeout);
-                } else if (execAsap) {
-                    func.apply(obj, args);
-                }
-
-                timeout = setTimeout(delayed, threshold || 100);
-            };
+    // debouncing function from John Hann
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    function debounce(func, threshold) {
+        var timeout;
+        return function () {
+            var obj = this, args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                func.apply(obj, args);
+            }, threshold || 100);
         };
+    }
 
-    $document.ready(function () {
+    // width/height attributes may be percentages ("100%"); only trust
+    // plain pixel values for computing an aspect ratio
+    function pixelAttribute(element, name) {
+        var value = element.getAttribute(name);
+        return /^\d+$/.test(value) ? parseInt(value, 10) : 0;
+    }
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+    document.addEventListener("DOMContentLoaded", function () {
+        var postContent = document.querySelector(".post-content"),
+            videoSelector = [
+                'iframe[src*="player.vimeo.com"]',
+                'iframe[src*="youtube.com"]',
+                'iframe[src*="youtube-nocookie.com"]',
+                'iframe[src*="kickstarter.com"][src*="video.html"]',
+                "object",
+                "embed"
+            ].join(", "),
+            supportsAspectRatio = "aspectRatio" in document.documentElement.style;
 
-        function updateImageWidth() {
-            var $this = $(this),
-                contentWidth = $postContent.outerWidth(), // Width of the content
-                imageWidth = this.naturalWidth; // Original image resolution
+        // Responsive video embeds (replaces jquery.fitvids.js):
+        // size each video embed to its declared aspect ratio at full column width
+        if (postContent) {
+            postContent.querySelectorAll(videoSelector).forEach(function (frame) {
+                var width = pixelAttribute(frame, "width"),
+                    height = pixelAttribute(frame, "height"),
+                    hasRatio = width > 0 && height > 0,
+                    wrapper = document.createElement("div");
 
-            if (imageWidth >= contentWidth) {
-                $this.addClass('full-img');
-            } else {
-                $this.removeClass('full-img');
-            }
+                wrapper.className = "fluid-width-video-wrapper";
+                frame.parentNode.insertBefore(wrapper, frame);
+                wrapper.appendChild(frame);
+
+                if (supportsAspectRatio) {
+                    frame.style.width = "100%";
+                    frame.style.height = "auto";
+                    frame.style.aspectRatio =
+                        hasRatio ? width + " / " + height : "16 / 9";
+                } else {
+                    // fitvids-style padding hack for browsers without aspect-ratio
+                    wrapper.style.position = "relative";
+                    wrapper.style.paddingTop =
+                        ((hasRatio ? height / width : 9 / 16) * 100) + "%";
+                    frame.style.position = "absolute";
+                    frame.style.top = "0";
+                    frame.style.left = "0";
+                    frame.style.width = "100%";
+                    frame.style.height = "100%";
+                }
+            });
         }
 
-        var $img = $("img").on('load', updateImageWidth);
-        function casperFullImg() {
-            $img.each(updateImageWidth);
+        // Mark images wider than the post column so CSS can break them out
+        if (postContent) {
+            var images = document.querySelectorAll("img");
+
+            var updateImageWidth = function (img) {
+                if (img.naturalWidth >= postContent.offsetWidth) {
+                    img.classList.add("full-img");
+                } else {
+                    img.classList.remove("full-img");
+                }
+            };
+
+            images.forEach(function (img) {
+                if (img.complete) {
+                    updateImageWidth(img);
+                } else {
+                    img.addEventListener("load", function () {
+                        updateImageWidth(img);
+                    });
+                }
+            });
+
+            window.addEventListener("resize", debounce(function () {
+                images.forEach(updateImageWidth);
+            }));
         }
 
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
+        // Smooth scroll for anchors with an optional pixel offset
+        // (replaces Arctic Scroll)
+        document.querySelectorAll(".scroll-down").forEach(function (link) {
+            link.addEventListener("click", function (event) {
+                var target = link.hash && document.querySelector(link.hash),
+                    offset = parseInt(link.getAttribute("data-offset"), 10) || 0,
+                    top;
 
-        $(".scroll-down").arctic_scroll();
-
-    });
-
-    // smartresize
-    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
+                if (!target) {
+                    return;
+                }
+                event.preventDefault();
+                top = target.getBoundingClientRect().top + window.pageYOffset + offset;
+                if ("scrollBehavior" in document.documentElement.style) {
+                    window.scrollTo({ top: top, behavior: "smooth" });
+                } else {
+                    window.scrollTo(0, top);
+                }
+            });
         });
-
-    };
-})(jQuery, 'smartresize');
+    });
+})();
